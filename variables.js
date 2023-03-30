300f49fc65c97b91ab735bfa4b69a9f0b82e1a31
MelissaPleitez <92410851+MelissaPleitez@users.noreply.github.com>
2023-03-30 15:15:10 -0600

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,6 @@
+export const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://melissaramirez0pleitez:userOne@cluster0.6vt1gc0.mongodb.net/OTI-Disabilities?retryWrites=true&w=majority"
+
+export const JWT_SECRETE = process.env.JWT_SECRETE || "palabrasupersecreta"
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:4000"
+export const MONGO_PORT = process.env.PORT || 4000
